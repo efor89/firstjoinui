0add8c39bb3bd7593d5bcdb8035e26edad928269
efor89 <67799203+efor89@users.noreply.github.com>
2021-04-19 06:46:48 +0200

    Update uitask.php

--- src/firstjoinui/task/uitask.php
+++ src/firstjoinui/task/uitask.php
@@ -32,120 +32,120 @@ class uitask extends Task{
 		$player = $this->player->getName();
         $this->plugin->openMyForm($this->player);
 		if ($config->exists("cmd")){
-		        if(!$plconfig->exists($player){
+		        if(!$plconfig->exists($player)){
 		    $this->plugin->getServer()->dispatchCommand($this->player, $config->get("cmd", ""));
 		}
 		       }
 		if ($config->exists("cmd1")){
-			if(!$plconfig->exists($player){
+			if(!$plconfig->exists($player)){
 		    $this->plugin->getServer()->dispatchCommand($this->player, $config->get("cmd1", ""));
 		}
 			}
 		if ($config->exists("cmd2")){
-			if(!$plconfig->exists($player){
+			if(!$plconfig->exists($player)){
 		    $this->plugin->getServer()->dispatchCommand($this->player, $config->get("cmd2", ""));
 		}
 	                }
 		if ($config->exists("cmd3")){
-			if(!$plconfig->exists($player){
+			if(!$plconfig->exists($player)){
 		    $this->plugin->getServer()->dispatchCommand($this->player, $config->get("cmd3", ""));
 		}
 			   }
 		if ($config->exists("cmd4")){
-			if(!$plconfig->exists($player){
+			if(!$plconfig->exists($player)){
 		    $this->plugin->getServer()->dispatchCommand($this->player, $config->get("cmd4", ""));
 		}
 			   }
 		if ($config->exists("cmd5")){
-			if(!$plconfig->exists($player){
+			if(!$plconfig->exists($player)){
 		    $this->plugin->getServer()->dispatchCommand($this->player, $config->get("cmd5", ""));
 		}
 			   }
 		if ($config->exists("cmd6")){
-			if(!$plconfig->exists($player){
+			if(!$plconfig->exists($player)){
 		    $this->plugin->getServer()->dispatchCommand($this->player, $config->get("cmd6", ""));
 		}
 			   }
 		if ($config->exists("cmd7")){
-			if(!$plconfig->exists($player){
+			if(!$plconfig->exists($player)){
 		    $this->plugin->getServer()->dispatchCommand($this->player, $config->get("cmd7", ""));
 		}
 			   }
 		if ($config->exists("cmd8")){
-			if(!$plconfig->exists($player){
+			if(!$plconfig->exists($player)){
 		    $this->plugin->getServer()->dispatchCommand($this->player, $config->get("cmd8", ""));
 		}
 			   }
 		if ($config->exists("cmd9")){
-			if(!$plconfig->exists($player){
+			if(!$plconfig->exists($player)){
 		    $this->plugin->getServer()->dispatchCommand($this->player, $config->get("cmd9", ""));
 		}
 			   }
 		if ($config->exists("ccmd")){
-			if(!$plconfig->exists($player){
+			if(!$plconfig->exists($player)){
 		    foreach($this->plugin->getServer()->getOnlinePlayers() as $player){
                 $this->plugin->getServer()->getCommandMap()->dispatch(new ConsoleCommandSender(), str_replace("{player}", '"'.$player->getName().'"', $config->get("ccmd", "")));
             }
 		}
 			   }
 		if ($config->exists("ccmd1")){
-			if(!$plconfig->exists($player){
+			if(!$plconfig->exists($player)){
 		    foreach($this->plugin->getServer()->getOnlinePlayers() as $player){
                 $this->plugin->getServer()->getCommandMap()->dispatch(new ConsoleCommandSender(), str_replace("{player}", '"'.$player->getName().'"', $config->get("ccmd1", "")));
             }
 		}
 			   }
 		if ($config->exists("ccmd2")){
-			if(!$plconfig->exists($player){
+			if(!$plconfig->exists($player)){
 		    foreach($this->plugin->getServer()->getOnlinePlayers() as $player){
                 $this->plugin->getServer()->getCommandMap()->dispatch(new ConsoleCommandSender(), str_replace("{player}", '"'.$player->getName().'"', $config->get("ccmd2", "")));
             }
 		}
 			   }
 		if ($config->exists("ccmd3")){
-			if(!$plconfig->exists($player){
+			if(!$plconfig->exists($player)){
 		    foreach($this->plugin->getServer()->getOnlinePlayers() as $player){
                 $this->plugin->getServer()->getCommandMap()->dispatch(new ConsoleCommandSender(), str_replace("{player}", '"'.$player->getName().'"', $config->get("ccmd3", "")));
             }
 		}
 			   }
 		if ($config->exists("ccmd4")){
-			if(!$plconfig->exists($player){
+			if(!$plconfig->exists($player)){
 		    foreach($this->plugin->getServer()->getOnlinePlayers() as $player){
                 $this->plugin->getServer()->getCommandMap()->dispatch(new ConsoleCommandSender(), str_replace("{player}", '"'.$player->getName().'"', $config->get("ccmd4", "")));
             }
 		}
 			   }
 		if ($config->exists("ccmd5")){
-			if(!$plconfig->exists($player){
+			if(!$plconfig->exists($player)){
 		    foreach($this->plugin->getServer()->getOnlinePlayers() as $player){
                 $this->plugin->getServer()->getCommandMap()->dispatch(new ConsoleCommandSender(), str_replace("{player}", '"'.$player->getName().'"', $config->get("ccmd5", "")));
             }
 		}
 			   }
 		if ($config->exists("ccmd6")){
-			if(!$plconfig->exists($player){
+			if(!$plconfig->exists($player)){
 		    foreach($this->plugin->getServer()->getOnlinePlayers() as $player){
                 $this->plugin->getServer()->getCommandMap()->dispatch(new ConsoleCommandSender(), str_replace("{player}", '"'.$player->getName().'"', $config->get("ccmd6", "")));
             }
 		}
 			   }
 		if ($config->exists("ccmd7")){
-			if(!$plconfig->exists($player){
+			if(!$plconfig->exists($player)){
 		    foreach($this->plugin->getServer()->getOnlinePlayers() as $player){
                 $this->plugin->getServer()->getCommandMap()->dispatch(new ConsoleCommandSender(), str_replace("{player}", '"'.$player->getName().'"', $config->get("ccmd7", "")));
             }
 		}
 			   }
 		if ($config->exists("ccmd8")){
-			if(!$plconfig->exists($player){
+			if(!$plconfig->exists($player)){
 		    foreach($this->plugin->getServer()->getOnlinePlayers() as $player){
                 $this->plugin->getServer()->getCommandMap()->dispatch(new ConsoleCommandSender(), str_replace("{player}", '"'.$player->getName().'"', $config->get("ccmd8", "")));
             }
 		}
 			   }
 		if ($config->exists("ccmd9")){
-			if(!$plconfig->exists($player){
+			if(!$plconfig->exists($player)){
 		    foreach($this->plugin->getServer()->getOnlinePlayers() as $player){
                 $this->plugin->getServer()->getCommandMap()->dispatch(new ConsoleCommandSender(), str_replace("{player}", '"'.$player->getName().'"', $config->get("ccmd9", "")));
             }
